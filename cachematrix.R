16b3c261a4cd3040ac7b639efd5024485892c70e
masslab <masslab@users.noreply.github.com>
2014-10-22 18:12:17 -0400

    Modified file with code and comments
    These functions permit creating a special matrix object, compute the
    inverse and cache the result.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions permit you to create a matrix object, compute its inverse,
+## and store (cache) the inverse in order to avoid having to do a potentially
+## costly matrix inversion computation each time.
+## 
+## Assumptions for usage: matrices are square and invertible
+##
 
-## Write a short comment describing this function
+## 'makeCacheMatrix' creates a special matrix object using the superassignment
+## operator such that it can cache its inverse (using the second function
+## 'cacheSolve' below)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ 
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(myMatrix) m <<- myMatrix
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
+  
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' computes the inverse of the matrix object created by
+## 'makeCacheMatrix' and checks to see if inverse was already cached before
+## doing so.  If so, it retrieves the cached values instead of computing the
+## inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m  
+  
 }
